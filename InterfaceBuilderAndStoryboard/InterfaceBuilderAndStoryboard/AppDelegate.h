e8daf95e988bf3d0edf1f5b5f66834e7729c67f9
JennyShalai <jenny.shalai@gmail.com>
2016-07-06 15:24:15 -0400

    done

---
+++ InterfaceBuilderAndStoryboard/InterfaceBuilderAndStoryboard/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  InterfaceBuilderAndStoryboard
+//
+//  Created by Flatiron School on 7/6/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
